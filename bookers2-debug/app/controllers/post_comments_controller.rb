3329025d0e27a5d9c731a80ef2d15877dd25fe23
小野楠奈 <onokusuna@onokusunokinanoMacBook-Pro.local>
2020-03-07 18:22:03 +0900

    re:Advance3

--- bookers2-debug/app/controllers/post_comments_controller.rb
+++ bookers2-debug/app/controllers/post_comments_controller.rb
@@ -1,4 +1,32 @@
 class PostCommentsController < ApplicationController
-	def create # 5 保存
+	def create
+	    @book = Book.find(params[:book_id])
+	    @post_comment = current_user.post_comments.new(post_comment_params)
+	    @post_comment.book_id = @book.id
+	    if @post_comment.save
+	       redirect_back(fallback_location: root_path)
+  		else
+  		   @book = Book.find(params[:book_id])
+  		   @user = @book.user
+  		   @books = Book.new
+  		   render "books/show"#kコントローラー/アクション
+  		end
+	end
+	def destroy
+	    @post_comment = current_user.post_comments.find(params[:id])#省略形(現在のuserに紐づいたpcommentモデルの中からbook.idを見つけた)
+	    @book = Book.find(params[:book_id])
+	    @post_comment.book_id = @book.id
+	    if @post_comment.destroy
+	       redirect_back(fallback_location: root_path)
+  		else
+  		   @book = Book.find(params[:book_id])
+  		   @user = @book.user
+  		   @books = Book.new
+  		   render "books/show"#kコントローラー/アクション
+  		end
+	end
+	private
+	def post_comment_params
+	    params.require(:post_comment).permit(:comment)
 	end
 end
